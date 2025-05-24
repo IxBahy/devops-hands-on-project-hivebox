35224b1e97331add52c996b5d3f33af2f8101939
ix-bahy <m.elbahy@buguard.io>
2025-05-24 22:06:35 +0300

    Remove async test client fixture from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import sys
 import os
-from httpx import AsyncClient
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -16,12 +15,3 @@ def client():
     Create a test client for FastAPI application
     """
     return TestClient(app)
-
-
-@pytest.fixture
-async def async_client():
-    """
-    Create an async test client for FastAPI application
-    """
-    async with AsyncClient(base_url="http://testserver") as client:
-        yield client
